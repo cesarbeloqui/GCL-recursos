214928f49e4000240efb5a58d5022da71d9e41c7
cesarbeloqui <120270198+cesarbeloqui@users.noreply.github.com>
2023-10-20 14:52:31 -0300

    Primer commit

---
+++ src/ui-component/cards/AuthFooter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// material-ui
+import Stack from '@mui/material/Stack';
+
+//-----------------------|| FOOTER - AUTHENTICATION 2 & 3 ||-----------------------//
+
+const AuthFooter = () => {
+    return (
+        <Stack direction="row" justifyContent="space-between">
+            <Typography variant="subtitle2" component={Link} href="https://berrydashboard.io" target="_blank" underline="hover">
+                berrydashboard.io
+            </Typography>
+            <Typography variant="subtitle2" component={Link} href="https://codedthemes.com" target="_blank" underline="hover">
+                &copy; codedthemes.com
+            </Typography>
+        </Stack>
+    );
+};
+
+export default AuthFooter;
